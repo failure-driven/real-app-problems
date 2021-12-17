c0297654b85c3b1b1ff3162e334f83883ef6d31d
Michael Milewski <saramic@gmail.com>
2021-12-17 22:29:00 +1100

    Add Page Object Model using site prism
    as per
      https://martinfowler.com/bliki/PageObject.html

--- rap/spec/features/user_tries_out_rap_spec.rb
+++ rap/spec/features/user_tries_out_rap_spec.rb
@@ -1,13 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "User tries out RAP", :js do
+  let(:home_page) { HomePage.new }
+
   it "User visits RAP" do
     When "user visits the site" do
-      visit root_path
+      home_page.load
     end
 
     Then "they see the RAP branding" do
-      expect(page.find("h1")).to have_text("RAP")
+      expect(home_page.title).to have_text("RAP")
     end
   end
 end
